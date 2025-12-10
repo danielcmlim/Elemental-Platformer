83eec202ad648e3561bac58f614623d8b25708af
daniellim2026 <daniel.lim2026@stgeorges.bc.ca>
2025-12-10 13:11:57 -0800

    movement and collisions to Box2D. Added dynamic body for player and static bodies for walls/level. Implemented edge fixtures for borders. ContactListener for collision events. Removed old manual collision code.

--- core/src/main/java/daniel/term1/Player.java
+++ core/src/main/java/daniel/term1/Player.java
@@ -7,118 +7,126 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
 
 public class Player {
+
+    private World world;
+    private Body body;
 
     private Texture idleSheet, runSheet, jumpSheet;
     private Animation<TextureRegion> idleAnim, runAnim, jumpAnim;
 
-    private float stateTime;
-    private String currentState = "IDLE";
+    private TextureRegion currentFrame;
+    private float stateTime = 0;
 
-    private Vector2 position;
-    private Vector2 velocity;
+    private float width = 38;     // sprite pixel width
+    private float height = 67;    // sprite pixel height
 
-    private float moveSpeed = 200f;
-    private float jumpPower = 550f;
-    private float gravity = -1000f;
-    private boolean isOnGround = false;
     private boolean isJumping = false;
-
-    private float scale = 1.5f;
     private boolean facingRight = true;
 
-    public Player() {
-        loadAnimations();
-        position = new Vector2(100, 150);
-        velocity = new Vector2(0, 0);
-        stateTime = 0f;
+    public Player(World world, float spawnX, float spawnY) {
+        this.world = world;
+
+        loadTextures();
+        createAnimations();
+        createBody(spawnX, spawnY);
     }
 
-    private void loadAnimations() {
-        idleSheet = new Texture(Gdx.files.internal("Player/Idle.png"));
-        runSheet = new Texture(Gdx.files.internal("Player/Run.png"));
-        jumpSheet = new Texture(Gdx.files.internal("Player/Jump.png"));
-
-        idleAnim = createAnimation(idleSheet, 5, 0.15f);
-        runAnim = createAnimation(runSheet, 8, 0.08f);
-        jumpAnim = createAnimation(jumpSheet, 3, 0.12f);
+    private void loadTextures() {
+        idleSheet = new Texture(Gdx.files.internal("player/Idle.png"));
+        runSheet = new Texture(Gdx.files.internal("player/Run.png"));
+        jumpSheet = new Texture(Gdx.files.internal("player/Jump.png"));
     }
 
-    private Animation<TextureRegion> createAnimation(Texture sheet, int frameCount, float frameDuration) {
-        int frameWidth = sheet.getWidth() / frameCount;
-        int frameHeight = sheet.getHeight();
-        TextureRegion[][] tmp = TextureRegion.split(sheet, frameWidth, frameHeight);
-        TextureRegion[] frames = new TextureRegion[frameCount];
-        for (int i = 0; i < frameCount; i++)
-            frames[i] = tmp[0][i];
-        return new Animation<>(frameDuration, frames);
+        private void createAnimations() {
+        int idleFrames = 7;
+        int runFrames = 8;
+        int jumpFrames = 9;
+
+        int frameW = idleSheet.getWidth() / idleFrames;
+        int frameH = idleSheet.getHeight();
+
+        TextureRegion[][] idleCut = TextureRegion.split(idleSheet, frameW, frameH);
+        TextureRegion[][] runCut = TextureRegion.split(runSheet, runSheet.getWidth() / runFrames, runSheet.getHeight());
+        TextureRegion[][] jumpCut = TextureRegion.split(jumpSheet, jumpSheet.getWidth() / jumpFrames, jumpSheet.getHeight());
+
+        idleAnim = new Animation<>(0.1f, idleCut[0]);
+        runAnim = new Animation<>(0.07f, runCut[0]);
+        jumpAnim = new Animation<>(0.12f, jumpCut[0]);
+
+        idleAnim.setPlayMode(Animation.PlayMode.LOOP);
+        runAnim.setPlayMode(Animation.PlayMode.LOOP);
+        jumpAnim.setPlayMode(Animation.PlayMode.LOOP);
     }
 
-    public void update(float delta) {
-        handleInput();
-        velocity.y += gravity * delta;
-        position.mulAdd(velocity, delta);
+    private void createBody(float spawnX, float spawnY) {
+        BodyDef bd = new BodyDef();
+        bd.type = BodyDef.BodyType.DynamicBody;
+        bd.position.set(spawnX / Main.PPM, spawnY / Main.PPM);
+        bd.fixedRotation = true;
 
-        if (position.y <= 140) {
-            position.y = 140;
-            velocity.y = 0;
-            isOnGround = true;
-            isJumping = false;
-        }
+        body = world.createBody(bd);
 
-        if (position.x < 0) position.x = 0;
-        if (position.x > Gdx.graphics.getWidth() - idleSheet.getWidth() * scale / 5f)
-            position.x = Gdx.graphics.getWidth() - idleSheet.getWidth() * scale / 5f;
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox((width / 2f) / Main.PPM, (height / 2f) / Main.PPM);
 
-        stateTime += delta;
+        FixtureDef fd = new FixtureDef();
+        fd.shape = shape;
+        fd.density = 1f;
+        fd.friction = 2.6f;
+
+        body.createFixture(fd).setUserData("player");
+        shape.dispose();
     }
 
-    private void handleInput() {
-        velocity.x = 0;
-        String newState = "IDLE";
+    public void update(float dt) {
+        stateTime += dt;
+        float moveSpeed = 4f;
+        Vector2 vel = body.getLinearVelocity();
+        Vector2 pos = body.getPosition();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
-            velocity.x = moveSpeed;
-            newState = "RUN";
-            facingRight = true;
-        } else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
-            velocity.x = -moveSpeed;
-            newState = "RUN";
+        boolean moving = false;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+            body.setLinearVelocity(-moveSpeed, vel.y);
             facingRight = false;
+            moving = true;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+            body.setLinearVelocity(moveSpeed, vel.y);
+            facingRight = true;
+            moving = true;
         }
 
-        if (Gdx.input.isKeyJustPressed(Input.Keys.UP) && isOnGround) {
-            velocity.y = jumpPower;
-            isOnGround = false;
+        // Jump
+        if (Gdx.input.isKeyJustPressed(Input.Keys.W) && Math.abs(vel.y) < 0.01f) { //ground check
+            body.applyLinearImpulse(new Vector2(0, 3f), pos, true);
             isJumping = true;
-            newState = "JUMP";
         }
+        if (Math.abs(vel.y) < 0.01f)
+            isJumping = false;
 
-        if (isJumping) newState = "JUMP";
-
-        currentState = newState;
+        // Pick animation
+        if (isJumping) {
+            currentFrame = jumpAnim.getKeyFrame(stateTime);
+        } else if (moving) {
+            currentFrame = runAnim.getKeyFrame(stateTime);
+        } else {
+            currentFrame = idleAnim.getKeyFrame(stateTime);
+        }
     }
 
     public void render(SpriteBatch batch) {
-        TextureRegion frame;
+        float drawX = body.getPosition().x * Main.PPM - width / 2f;
+        float drawY = body.getPosition().y * Main.PPM - height / 2f;
 
-        switch (currentState) {
-            case "RUN":
-                frame = runAnim.getKeyFrame(stateTime, true);
-                break;
-            case "JUMP":
-                frame = jumpAnim.getKeyFrame(stateTime, true);
-                break;
-            default:
-                frame = idleAnim.getKeyFrame(stateTime, true);
-                break;
+        if (facingRight) {
+            batch.draw(currentFrame, drawX, drawY, width, height);
+        } else {
+            batch.draw(currentFrame, drawX + width, drawY, -width, height);
         }
-
-        if (!facingRight && !frame.isFlipX()) frame.flip(true, false);
-        if (facingRight && frame.isFlipX()) frame.flip(true, false);
-
-        batch.draw(frame, position.x, position.y, frame.getRegionWidth() * scale, frame.getRegionHeight() * scale);
     }
 
     public void dispose() {
